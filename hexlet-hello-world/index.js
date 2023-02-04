abc0b7497f2cef74bf4aeecdcb420eb267305e9f
Svetlana <pidgin66@gmail.com>
2023-02-04 22:20:57 +0300

    fix index.js

--- hexlet-hello-world/index.js
+++ hexlet-hello-world/index.js
@@ -1,6 +1,7 @@
-import _ from "lodash";
+import _ from 'lodash';
 
+// eslint-disable-next-line no-console
 console.log('Hello, Hexlet');
 
+// eslint-disable-next-line no-console
 console.log(_.last(['one', 'two']));
-
